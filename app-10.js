5ad83785149f3cec140192bdc84c2756a9f1b8e1
flads <flads.me@gmail.com>
2022-02-27 23:21:07 -0300

    feat: solve problem 10

---
+++ app-10.js
@@ -0,0 +1,20 @@
+/**
+ * Problem 10.
+ * 
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+import { isPrime } from "./helpers.js";
+
+const number = 2000000;
+const primes = [];
+
+for (let i = 0; i < number; i++) {
+    if (isPrime(i)) {
+        primes.push(i);
+    }
+}
+
+const primesSum = primes.reduce((x, y) => x + y, 0);
+
+console.log(primesSum);
